85670360082b6fada5a24980ce44171650660c7f
Ashley Ramsey <dusty1boy1984@gmail.com>
2022-12-26 18:11:22 -0500

    Corrected typos

--- src/App.js
+++ src/App.js
@@ -6,24 +6,30 @@ import RecipeData from "./RecipeData"
 
 function App() {
   const [recipes, setRecipes] = useState(RecipeData);
-
-  const addRecipe = (data) => {
-    setRecipes([...recipes, data]);
-  };
-
-  const deleteRecipe = (recipeToDelete) => {
-    setRecipes((currentRecipes) => currentRecipes.filter((recipe, index) => index !== recipeToDelete));
-  };
+  
+//   setRecipes((prev) => [...prev, newRecipe])
+  const addRecipes = (data) => {
+    setRecipes([...recipes, data])
+  }
+  
+  // const newRecipe = {name: sdfkljasdf, cui}
 
   // TODO: Add the ability for the <RecipeList /> component to list and delete an existing recipe.
+  // index !== length -1?
+  const deleteRecipe = (recipeToDelete) => {
+    setRecipes((currentRecipes) => currentRecipes.filter((recipe, index) => index !== recipeToDelete))
+  }
+  
+  
   // TODO: Add the ability for the <RecipeCreate /> component to create new recipes.
 
+  
   
   return (
     <div className="App">
       <header><h1>Delicious Food Recipes</h1></header>
-      <RecipeList />
-      <RecipeCreate />
+      <RecipeList recipes={recipes} deleteRecipe={deleteRecipe} />
+      <RecipeCreate addRecipes={addRecipes} />
     </div>
   );
 }
